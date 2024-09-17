6e7180d18841ef60c3cdb2bdf884b34c678bdee1
willer-barros <willerbarros137@gmail.com>
2024-09-16 23:13:02 -0300

    primeiro commit

---
+++ clientes/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class ClienteManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Você deve passar ao menos um email')
+        
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None):
+        user = self.create_superuser(email, password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+    
+class Cliente(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    nome = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = ClienteManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perm(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
